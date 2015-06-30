8e37641291b01aef269ae004542ac2eeeb69358d
Romesz <firtermaiszter.romano@gmail.com>
2015-06-30 18:47:58 +0200

    add koa-router.js

--- koa-router.js
+++ koa-router.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var koa = require('koa');
 var render = require('koa-ejs');
+var router = require('koa-route');
 var logger = require('koa-logger')
 
 var app = koa();
@@ -18,18 +19,16 @@ render(app, {
   debug: true
 });
 
-app.use(function *(next) {
-  console.log('I can do amazing things here BEFORE the view is served!');
-  yield next;
-});
-
-
-app.use(function *(){
-  //this.body = 'Hello Koaaaaa';
+app.use(router.get('/', function *() {
+  var greet = 'This string is served form a Koa GET request!';
   yield this.render('hellokoa', {
-    greet_koa: 'helooo Koaaa'
+    greet_koa: greet
   });
-});
+}));
+
+app.use(router.get('*', function *() {
+  this.body = '404 page does not found';
+}));
 
 /*
 * Error handling
@@ -41,12 +40,3 @@ app.on('error', function(error) {
 app.listen(3000);
 
 console.log('Server listening on port 3000');
-
-
-/*
-Check moduls:
-"koa-basic-auth": "^1.1.1",
-"koa-csrf": "^2.1.1",
-"koa-session": "^3.1.0",
-"koa-static": "^1.4.5",
-*/
